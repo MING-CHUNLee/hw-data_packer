667e2881195ad4693d91a2038fe0240c5dbd8e77
MING-CHUNLee <mindy80230@gmail.com>
2025-03-06 15:21:47 +0800

    hw-data_packer

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -8,13 +8,25 @@ module ShortStringPacker
   # Returns: a Integer object
   def self.pack(str)
     # IMPLEMENT THIS METHOD
+    # Convert string to bytes and reduce to a single integer
+    str.bytes.reduce(0) { |acc, byte| (acc << 8) + byte }
   end
 
   ## Unpacks a Integer from pack() method into a short string
   # Arguments:
-  #   packed - a Integer object
+  # packed - a Integer object
   # Returns: a String object
   def self.unpack(packed)
     # IMPLEMENT THIS METHOD
+    # Convert integer to bytes and join to form a string
+    strs = []
+    # Loop through integer and convert to bytes
+    while packed.positive?
+      # Add byte to front of array
+      strs.unshift((packed & 0xFF).chr)
+      # Shift integer to the right by 8 bits
+      packed >>= 8
+    end
+    strs.join
   end
 end
